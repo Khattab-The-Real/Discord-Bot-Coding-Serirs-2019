77757fdb59d57c063dd2bdf1152a18970f7bd0dd
Khattab Yousef <53058096+Khattab-The-Real@users.noreply.github.com>
2019-07-19 14:36:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,40 @@ bot.on("message", async message => {
     let command = messageArray[0];
     let args = messageArray.slice(1);
 
-    
-    if(command === `${prefix}test`){
-     message.channel.send("This test command is working perfectly!")   
+    if(command === `${prefix}botinfo`){
+       let bicon = bot.user.displayAvatarURL; 
+
+       let botEmbed = new Discord.RichEmbed()
+       .setTitle("Bot Information")
+        .setDescription("This is the Bots info")
+        .setColor("#51f542")
+        .setThumbnail(bicon)
+        .addField("Bot Name", bot.user.username)
+        .addField("Date Created", bot.user.createdAt);
+
+
+       message.channel.send(botEmbed)
     };
+
+
+    if(command === `${prefix}serverinfo`){
+        let sicon = message.guild.displayAvatarURL;
+        let serverEmbed = new Discord.RichEmbed()
+        .setTitle("Server Information")
+        .setDescription("This is server info")
+        .setColor("#6042f5")
+        .setThumbnail(sicon)
+        .addField("Server Name", message.guild.name)
+        .addField("Created at", message.guild.createdAt)
+        .addField("You Joined At", message.member.joinedAt)
+        .addField("Total Members", message.guild.memberCount)
+        .addField("Your Username", message.author.tag);
+
+
+        message.channel.send(serverEmbed)
+    }
+
+   
     
 });
 
