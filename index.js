d63d269183aa744a826f3211ce6b1e684e40dc5f
Khattab Yousef <53058096+Khattab-The-Real@users.noreply.github.com>
2019-08-04 13:43:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
-const Discord = require('discord.js');
-const botconfig = require('./botconfig.json');
-const tokenfile = require('./tokenfile.json');
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on('ready', async () => {
-    console.log(`${bot.user.username} Is online`);
-    bot.user.setActivity('Khattab on Youtube', {type: 'WATCHING'});
+bot.on(`ready`, async () => {
+  console.log(`${bot.user.username} Is online!`)
+  bot.user.setActivity("Testing Myself for a test!")
 });
 
 
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return message.reply('Nothing works here :)');
+bot.on("message", async message =>{
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return message.reply("I can not respond to Dm's!");
 
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if(command === `${prefix}botinfo`){
-       let bicon = bot.user.displayAvatarURL; 
-
-       let botEmbed = new Discord.RichEmbed()
-       .setTitle("Bot Information")
-        .setDescription("This is the Bots info")
-        .setColor("#51f542")
-        .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username)
-        .addField("Date Created", bot.user.createdAt);
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
 
-       message.channel.send(botEmbed)
-    };
+  if(cmd === `${prefix}kick`){
+    //!kick @Noob#4567 asking for it
 
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send(`${message.author.username} Can not find that user`);
+    let kReason = args.join(" ").slice(22);
+    //We are going to check if the person sending the command has Permissions to do so
+    if(!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.channel.send(`${message.author.username} you do not have Proper Permissions to do that! `);
+    if(kUser.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.channel.send(`No can do pal, they are a staff member`);
 
-    if(command === `${prefix}serverinfo`){
-        let sicon = message.guild.displayAvatarURL;
-        let serverEmbed = new Discord.RichEmbed()
-        .setTitle("Server Information")
-        .setDescription("This is server info")
-        .setColor("#6042f5")
-        .setThumbnail(sicon)
-        .addField("Server Name", message.guild.name)
-        .addField("Created at", message.guild.createdAt)
-        .addField("You Joined At", message.member.joinedAt)
-        .addField("Total Members", message.guild.memberCount)
-        .addField("Your Username", message.author.tag);
+    let kickEmbed = new Discord.RichEmbed()
+    .setTitle("Kicked a User")
+    .setDescription("This is a message to let people know someone is Kicked :tada:")
+    .setColor("#ff0000")
+    .addField("Kicked User:", `${kUser} With ID: ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.username}> With ID: ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Time", mssage.createdAt)
+    .addField("Reason", kReason);
 
+    let kickChannel = message.guild.channels.find(`name`, "incidents");
+    if(!kickChannel) return message.reply("No incidents channel Found!")
 
-        message.channel.send(serverEmbed)
-    }
+    message.guild.member(kUser).kick(kReason)
+    kickChannel.send(kickEmbed);
 
-   
-    
+  }
+
+  if(cmd === `${prefix}ban`){
+    //!kick @Noob#4567 asking for it
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send(`${message.author.username} Can not find that user`);
+    let bReason = args.join(" ").slice(22);
+    //We are going to check if the person sending the command has Permissions to do so
+    if(!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.channel.send(`${message.author.username} you do not have Proper Permissions to do that! `);
+    if(bUser.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.channel.send(`No can do pal, they are a staff member`);
+
+    let banEmbed = new Discord.RichEmbed()
+    .setTitle("banned a user")
+    .setDescription("This is a message to let people know someone is Banned :tada:")
+    .setColor("#ff0000")
+    .addField("Banned User:", `${kUser} With ID: ${kUser.id}`)
+    .addField("banned By", `<@${message.author.username}> With ID: ${message.author.id}`)
+    .addField("banned In", message.channel)
+    .addField("Time", mssage.createdAt)
+    .addField("Reason", kReason);
+
+    let banChannel = message.guild.channels.find(`name`, "incidents");
+    if(!banChannel) return message.reply("No incidents channel Found!")
+
+    message.guild.member(bUser).bick(bReason)
+    banChannel.send(banEmbed);
+
+  }
+
 });
 
-
-bot.login(tokenfile.token);
-
-
+bot.login(botconfig.token);
